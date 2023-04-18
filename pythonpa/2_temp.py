d29c1ae5ea7b2c70912f181b094b51d3826ea07c
Bhumika D M <129480613+Bhumikadm-02@users.noreply.github.com>
2023-04-18 15:33:31 +0530

    Delete pythonpa directory

--- pythonpa/2_temp.py
+++
@@ -1,8 +0,0 @@
-#input temperature in celsius
-celsius = float(input("Enter temperature in Celsius: "))
-
-#celsius to fahrenheit
-fahrenheit = (celsius*9/5)+32
-
-# Print result
-print(f"{celsius} degrees Celsius is equal to {fahrenheit} degrees Fahrenheit.")
